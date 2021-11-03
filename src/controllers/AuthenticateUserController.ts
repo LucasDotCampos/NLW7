eae8622e130dce38684725a40ad975060494cd48
Lucas Campos <lucasdotcampos@gmail.com>
2021-11-02 20:32:15 -0400

    ultimo commit

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserService } from '../services/AuthenticateUserService';
 
 class AuthenticateUserController {
     async handle(request: Request, response: Response) {
+        
         const { code } = request.body;
         const service = new AuthenticateUserService();
         const result = await service.execute(code);
@@ -14,12 +15,13 @@ class AuthenticateUserController {
         response.redirect(
             `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
         );
-            
+            console.log('chegou na rota get');
     }
     async github(request: Request, response: Response) {
         const { code } = request.query;
 
         return response.json(code);
+        console.log('chegou na query')
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 
 app.use(routes);
 
-const port = process.env.PORT|| 4000
+const PORT = 4000
 
-app.listen(port, () => console.log(`server is running on ${port}`))
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
